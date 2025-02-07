f667c62913ca6f65b2ca744c6e2d73a234b33be4
Aayush <Aayush00222@gmail.com>
2025-02-07 18:53:23 +0530

    update

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  return res.json({ status: true, message: "last time code update" });
+  return res.json({ status: true, message: "final code update" });
 });
 
 app.listen(5000, () => {
